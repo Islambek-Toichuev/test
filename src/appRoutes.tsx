a945ac9bdd486f3eb69f11e9010d71ede0d531e9
Islambek-Toichuev <32975125+Islambek-Toichuev@users.noreply.github.com>
2019-03-15 15:45:05 +0600

    Update appRoutes.tsx

--- src/appRoutes.tsx
+++ src/appRoutes.tsx
@@ -1,33 +1,40 @@
 import React, {Component} from "react";
-import {Route, Switch, Router } from "react-router-dom";
+import {Route, Switch, Router} from "react-router-dom";
 import Login from "./pages/publicPages/login";
 import SignUp from "./pages/publicPages/signUp";
 import TradeScreen from "./pages/tradeScreen";
 import AdminPage from "./pages/adminPage";
 import createHistory from "history/createBrowserHistory";
 import Menu from "./components/menu";
+import {SessionContext} from "./App";
 
 const FrontPage = React.lazy(() => import('./pages/frontPage'));
 const history = createHistory();
+
 class AppRoutes extends Component {
     render() {
         return (
-                <Router history={history}>
-                    <div>
-                        <Menu history={history}/>
-                        <React.Suspense fallback={<div/>}>
-                            <Switch>
-                                <Route path="/currencies/:PrimaryCurrencyCode" component={TradeScreen}/>
-                                <Route path="/currencies" render={(props) => <FrontPage {...props} />}/>
-                                <Route path="/admin" component={AdminPage} />
-                                <Route path="/signup" component={SignUp}/>
-                                <Route path="/login" component={Login}/>
-                                <Route path="/" exact component={Login}/>
-                            </Switch>
-                        </React.Suspense>
-                    </div>
-                </Router>
-        );
+            <SessionContext.Consumer>
+                {({session }) => (
+                    <Router history={history}>
+                        <div>
+                            <Menu history={history}/>
+                            <React.Suspense fallback={<div/>}>
+                                <Switch>
+                                    {session && <Route path="/currencies/:PrimaryCurrencyCode" component={TradeScreen}/>}
+                                    {session && <Route path="/currencies" render={(props) => <FrontPage {...props} />}/>}
+                                    {session && <Route path="/admin" component={AdminPage}/>}
+                                    <Route path="/signup" component={SignUp}/>
+                                    <Route path="/login" component={Login}/>
+                                    <Route path="/" exact component={Login}/>
+                                    <Route component={Login} />
+                                </Switch>
+                            </React.Suspense>
+                        </div>
+                    </Router>
+                )}
+            </SessionContext.Consumer>
+        )
     }
 }
 
