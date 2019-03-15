361f5b45b1d43904ac0abe0f552ed1f7b8788d1e
Islambek-Toichuev <32975125+Islambek-Toichuev@users.noreply.github.com>
2019-03-15 13:11:44 +0600

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
+import "./App.css";
 import React, { Component } from 'react'
 import AppRoutes from "./appRoutes";
-import "./App.css";
+import AuthorizationService from "./services/authorization";
+
+const authService = new AuthorizationService();
+
+export const SessionContext = React.createContext({
+    session: authService.getCurrentSession(),
+    changeSession: (session:string) => {}
+});
 
 export default class App extends Component {
-  render() {
+
+    newSession = (session: string) => this.setState({session: session});
+
+    state = {
+        session: authService.getCurrentSession(),
+        changeSession: (session: string) => this.newSession(session)
+    };
+
+    render() {
     return (
       <div className="app">
-          <AppRoutes />
+          <SessionContext.Provider value={this.state}>
+              <AppRoutes />
+          </SessionContext.Provider>
       </div>
     );
   }
