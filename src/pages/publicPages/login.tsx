f0560e3602edcb2c6b283aaaf69cee2121b5cbcb
Islambek-Toichuev <32975125+Islambek-Toichuev@users.noreply.github.com>
2019-03-15 13:10:55 +0600

    Update login.tsx

--- src/pages/publicPages/login.tsx
+++ src/pages/publicPages/login.tsx
@@ -1,70 +1,101 @@
-import React, { Component } from 'react'
-import AuthorizationService from "../../services/authorization";
+import React, {Component} from 'react'
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-import { Link } from 'react-router-dom';
-import { Redirect } from 'react-router';
+import {Link} from 'react-router-dom';
+import {Redirect} from 'react-router';
+import {Validators} from "../../helpers/validators";
+import AuthorizationService from "../../services/authorization";
+import { SessionContext } from "../../App";
 import "./style.css";
 
+const validate = new Validators();
 const authService = new AuthorizationService();
 
-export default class Login extends Component {
-  state = {
-    email: '',
-    password: '',
-    error: false,
-    loggedIn: false
-  };
+interface Props {
+    history: any,
+}
 
-  onChange = (val: string, field: string) => {
-    this.setState({ [field]: val, error: false })
-  };
+export default class Login extends Component<Props> {
+    state = {
+        email: '',
+        password: '',
+        error: false,
+    };
 
-  logIn = () => {
-    let authorize = authService.logIn(this.state);
-    if (!authorize.res) return this.setState({ error: true })
-    return this.setState({ loggedIn: true })
-  };
+    onChange = (val: string, field: string) => {
+        if (field === 'email') {
+            return validate.email(val) ?
+                this.setState({email: val, error: false}) :
+                this.setState({error: true});
+        }
+        if (field === 'password') {
+            this.setState({password: val});
+            return val.length >= 6 ?
+                this.setState({error: false}) :
+                this.setState({error: true});
+        }
+    };
 
-  render() {
-    return (
-      <div className="container-login">
-        <div className="login_form">
-          <h2>Login</h2>
-          <form>
-            <TextField
-              label="Email"
-              margin="normal"
-              className="login_field"
-              error={this.state.error}
-              onChange={(e) => this.onChange(e.target.value, 'email')}
-            />
-            <TextField
-              label="Password"
-              type="password"
-              margin="normal"
-              className="login_field"
-              error={this.state.error}
-              onChange={(e) => this.onChange(e.target.value, 'password')}
-            />
-            <div className="actions">
-              <Button
-                onClick={() => this.logIn()}
-                variant="contained"
-                color="primary"
-              >
-                Login
-              </Button>
-              <Link to="/signup">
-                <Button>
-                  Sign Up
-                </Button>
-              </Link>
-            </div>
-          </form>
-        </div>
-        {this.state.loggedIn && <Redirect to="/currencies" />}
-      </div>
-    )
-  }
+    logIn = (changeSession: any) => {
+        let {email, password} = this.state;
+
+        let authorize = authService.logIn({email: email, password: password});
+        if (!authorize.res) return this.setState({error: true});
+        changeSession(`{email: ${email}, password: ${password},}`);
+        return  this.props.history.push('/currencies');
+    };
+
+    render() {
+        let {
+            email,
+            password,
+            error,
+        } = this.state;
+
+        return (
+            <SessionContext.Consumer>
+                {({session, changeSession}) => (
+                    <div className="container-login">
+                        <div className="login_form">
+                            <h2>Login</h2>
+                            <form>
+                                <TextField
+                                    label="Email"
+                                    margin="normal"
+                                    className="login_field"
+                                    error={error}
+                                    onChange={(e) => this.onChange(e.target.value, 'email')}
+                                />
+                                {!validate.email(email) && <span className="error">Please provide valid email</span>}
+                                <TextField
+                                    label="Password"
+                                    type="password"
+                                    margin="normal"
+                                    className="login_field"
+                                    error={error}
+                                    onChange={(e) => this.onChange(e.target.value, 'password')}
+                                />
+                                {password.length <= 5 && <span className="error">Min password length is 6</span>}
+                                <div className="actions">
+                                    <Button
+                                        onClick={() => this.logIn(changeSession)}
+                                        variant="contained"
+                                        color="primary"
+                                        disabled={(!validate.email(email) || password.length <= 5)}
+                                    >
+                                        Login
+                                    </Button>
+                                    <Link to="/signup">
+                                        <Button>
+                                            Sign Up
+                                        </Button>
+                                    </Link>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                )}
+            </SessionContext.Consumer>
+        )
+    }
 }
